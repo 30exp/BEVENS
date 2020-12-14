d2e9979d18b2cbf3b13b370b85b30526e89e0a43
30exp <60365260+30exp@users.noreply.github.com>
2020-12-14 14:47:04 -0500

    edits-1

--- js/app.js
+++ js/app.js
@@ -52,3 +52,20 @@ $('h4').click(function(){
     $(this).next().addClass('more');
   }
 })
+
+$('.form-button input').click(function(e){
+  // do not submit form
+  e.preventDefault();
+  // save value if the form field is not blank or the default
+  if( $('#email').val() !== '' && $('#email').val() !== 'EMAIL' ){
+    var val = $('#email').val();
+    // add email address to message
+    $('.form-message span').text(val);
+    // show message (initially hidden in CSS)
+    $('.form-message').css('display','grid');
+  }
+})
+
+$('.form-message').click(function(){
+  $('.form-message').hide();
+})
